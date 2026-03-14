fa34a2fe8629f9cf37ecac29bf87c6f99c182e12
sammyfischer <sammyfischer50@gmail.com>
2026-03-13 21:03:13 -0400

    prevented running start from non-base branch

--- src/cli/start.rs
+++ src/cli/start.rs
@@ -1,6 +1,7 @@
 //! Start subcommand
 
-use crate::cli::{Cli, CliResult};
+use crate::cli::error::CliError;
+use crate::cli::{Cli, CliResult, get_current_branch};
 use crate::{await_child, git};
 
 #[derive(clap::Args, Clone, Debug)]
@@ -17,6 +18,13 @@ pub struct Args {
 impl Args {
   pub fn run(&self, cli: &Cli) -> CliResult {
     let sep = self.sep.as_ref().unwrap_or(&cli.config.branch_sep);
+
+    let current_branch = get_current_branch()?;
+    if !cli.config.protected_branches.contains(&current_branch) {
+      return Err(CliError::Generic(
+        "Must call start from a base branch. You can modify base branches with the `feature config` command.".into(),
+      ));
+    }
 
     let branch_name = self.words.join(sep);
     println!("Creating branch: {}", branch_name);
