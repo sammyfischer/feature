2d3b186d42a34410a86e9df272f01c27caec0d05
sammyfischer <sammyfischer50@gmail.com>
2026-03-30 14:22:25 -0400

    removed redundant fetching in sync

--- src/cli/sync.rs
+++ src/cli/sync.rs
@@ -1,7 +1,7 @@
 use anyhow::{Context, Result, anyhow};
 use git2::FetchOptions;
 
-use crate::cli::{Cli, fetch_all, get_current_branch, get_remote_callbacks, has_local_changes};
+use crate::cli::{Cli, get_current_branch, get_remote_callbacks, has_local_changes};
 use crate::open_repo;
 
 pub const LONG_ABOUT: &str = r"Updates all base branches with their remotes.
@@ -11,7 +11,6 @@ Fast-forwards local branches (e.g. refs/heads/*) and force-updates remotes
 
 pub fn run(cli: &Cli) -> Result<()> {
   let repo = open_repo!();
-  fetch_all(&repo).context("Failed to fetch all remotes")?;
 
   if has_local_changes(&repo).context("Failed to determine if there are any local changes")? {
     return Err(anyhow!(
