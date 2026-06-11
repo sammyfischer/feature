eb3f3127de996ae960010228f43db8dd1faaa730
Sammy Fischer <sammyfischer50@gmail.com>
2026-06-11 18:35:03 -0400

    feat(sync): add dedicated progress spinner for each repo

--- src/cli/sync.rs
+++ src/cli/sync.rs
@@ -16,7 +16,7 @@ use git2::{
   Repository,
   SubmoduleUpdateOptions,
 };
-use indicatif::{ProgressBar, ProgressStyle};
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 use crate::cli::prune::prune_branches;
@@ -25,9 +25,9 @@ use crate::config::{self, Config};
 use crate::util::branch::{fetch_all, get_current_branch_name, hard_reset};
 use crate::util::branch_meta::BranchMeta;
 use crate::util::diff::{DiffSummary, has_workdir_changes};
+use crate::util::get_credentials_cb;
 use crate::util::string::ToStrLossyOwned;
 use crate::util::term::TICK_STRINGS;
-use crate::util::{get_credentials_cb, open_repo_from_dirs};
 use crate::{App, data, style};
 
 const LONG_ABOUT: &str = r"Updates all branches with their remotes (if they have one), then prunes merged
@@ -81,11 +81,7 @@ pub enum SyncAction {
   /// For projects, the repo was cloned
   ProjectInit,
 
-  ModuleInit,
-  ModuleUpdate {
-    old: String,
-    new: String,
-  },
+  ModuleUpdate,
 }
 
 /// What happened in an attempt to sync a particular branch in a repo
@@ -135,18 +131,40 @@ impl Args {
     let app_config = &state.config;
     let git_config = state.repo.config()?.snapshot()?;
 
+    // progress bars
+    let multi = MultiProgress::new();
+    let mut prefix_width: usize;
+
+    let main_progress = {
+      let name = work_dir
+        .unwrap_or(&repo_dir)
+        .file_name()
+        .map(|name| name.to_string_lossy().to_string())
+        .unwrap_or_else(|| "repo".to_string());
+
+      prefix_width = name.len();
+      let spinner = add_spinner(&multi, name.clone());
+      (name, spinner)
+    };
+
+    // project names and bars
     let skip_projects = match self.no_projects {
       Some(it) => it,
       None => !data::get_sync_projects(&git_config)?,
     };
-    let proj_names: Vec<_> = if skip_projects {
+    let proj_progresses: Vec<_> = if skip_projects {
       Vec::new()
     } else {
       state
         .config
         .projects
         .iter()
-        .map(|(name, _)| name.to_owned())
+        .map(|(name, _)| {
+          let name = name.to_owned();
+          prefix_width = name.len().max(prefix_width);
+
+          (name.clone(), add_spinner(&multi, name))
+        })
         .collect()
     };
 
@@ -154,41 +172,47 @@ impl Args {
       Some(it) => it,
       None => !data::get_sync_modules(&git_config)?,
     };
-    let mod_names: Vec<_> = if skip_modules {
+    let mod_progresses: Vec<_> = if skip_modules {
       Vec::new()
     } else {
       state
         .repo
         .submodules()?
         .iter()
-        .map(|module| module.name_bytes().to_str_lossy_owned())
+        .map(|module| {
+          let name = module.name_bytes().to_str_lossy_owned();
+          prefix_width = name.len().max(prefix_width);
+
+          (name.clone(), add_spinner(&multi, name))
+        })
         .collect()
     };
 
-    thread::scope(|scope| -> Result<_> {
-      // begin spinner
-      let progress = ProgressBar::new_spinner().with_style(
-        ProgressStyle::with_template("{spinner:.cyan} {msg} {elapsed}")?
-          .tick_strings(&TICK_STRINGS),
-      );
-      progress.set_message("Syncing:");
-      progress.enable_steady_tick(Duration::from_millis(100));
+    // set spinner templates
+    set_spinner_style(&main_progress.1, prefix_width);
+    for (_, progress) in &proj_progresses {
+      set_spinner_style(progress, prefix_width);
+    }
+    for (_, progress) in &mod_progresses {
+      set_spinner_style(progress, prefix_width);
+    }
 
-      // SYNC PARENT REPO
-      let repo_thread = scope.spawn(|| {
-        let repo = open_repo_from_dirs(&repo_dir, work_dir)?;
-        self.sync_repo(&repo, app_config)
-      });
+    // main repo must run first, since projects/submodule state could change as a
+    // result
+    let main_result = self.sync_repo(&state.repo, app_config, &main_progress.1)?;
 
+    let (proj_results, mod_results) = thread::scope(|scope| -> Result<_> {
       // SYNC ALL PROJECTS
       let proj_thread = scope.spawn(|| {
         if skip_projects {
           Vec::new()
         } else {
-          app_config
-            .projects
+          proj_progresses
             .par_iter()
-            .map(|(_, project)| self.sync_or_clone_project(project))
+            .map(|(name, progress)| {
+              let project = &app_config.projects[name];
+              self.sync_or_clone_project(project, progress)
+            })
             .collect()
         }
       });
@@ -198,9 +222,9 @@ impl Args {
         if skip_modules {
           Vec::new()
         } else {
-          mod_names
+          mod_progresses
             .par_iter()
-            .map(|mod_name| -> Result<Option<SyncAction>> {
+            .map(|(name, progress)| -> Result<SyncAction> {
               let repo = match &work_dir {
                 Some(work_dir) => {
                   let repo = Repository::open_bare(&repo_dir)?;
@@ -209,56 +233,61 @@ impl Args {
                 }
                 None => Repository::open(&repo_dir)?,
               };
-              self.update_module(&repo, mod_name)
+              self.update_module(&repo, name, progress)
             })
             .collect()
         }
       });
 
       // collect and display
-      let main_result = repo_thread.join().unwrap();
       let proj_results = proj_thread.join().unwrap();
       let mod_results = mod_thread.join().unwrap();
-      progress.finish_with_message("Synced in");
 
-      let repo_name = work_dir
-        .unwrap_or(&repo_dir)
-        .file_name()
-        .map(|name| name.to_string_lossy().to_string())
-        .unwrap_or_else(|| "repo".to_string());
+      Ok((proj_results, mod_results))
+    })?;
 
-      match main_result {
-        Ok(action) => println!("{}", display_sync_action(&repo_name, &action)),
-        Err(e) => eprintln!(
-          "{} to sync {}: {}",
-          style("Failed"),
-          style(&repo_name).cyan(),
-          e
-        ),
-      }
+    // main repo summary
+    if let SyncAction::Sync(updates) = &main_result {
+      let out = display_sync_updates(&main_progress.0, updates);
+      if !out.is_empty() {
+        println!("{}", out)
+      };
+    }
 
-      for result in proj_names.iter().zip(proj_results) {
-        let (name, result) = result;
-        match result {
-          Ok(action) => println!("{}", display_sync_action(name, &action)),
-          Err(e) => eprintln!("{} to sync {}: {}", style("Failed"), style(name).cyan(), e),
+    // project summaries/errors
+    for result in proj_progresses.iter().zip(proj_results) {
+      let ((name, _), result) = result;
+      match result {
+        Ok(SyncAction::Sync(updates)) => {
+          let out = display_sync_updates(name, &updates);
+          if !out.is_empty() {
+            println!("{}", out);
+          }
         }
+        Err(e) => eprintln!("{} to sync {}: {}", style("Failed"), style(name).cyan(), e),
+        _ => (),
       }
+    }
 
-      for result in mod_names.iter().zip(mod_results) {
-        let (name, result) = result;
-        match result {
-          Ok(Some(action)) => println!("{}", display_sync_action(name, &action)),
-          Ok(None) => (),
-          Err(e) => eprintln!("{} to sync {}: {}", style("Failed"), style(name).cyan(), e),
-        }
+    // submodule errors
+    for result in mod_progresses.iter().zip(mod_results) {
+      let ((name, _), result) = result;
+      if let Err(e) = result {
+        eprintln!("{} to sync {}: {}", style("Failed"), style(name).cyan(), e)
       }
+    }
 
-      Ok(())
-    })
+    Ok(())
   }
 
-  fn sync_repo(&self, repo: &Repository, config: &Config) -> Result<SyncAction> {
+  fn sync_repo(
+    &self,
+    repo: &Repository,
+    config: &Config,
+    progress: &ProgressBar,
+  ) -> Result<SyncAction> {
+    progress.enable_steady_tick(Duration::from_millis(100));
+
     let git_config = repo.config()?.snapshot()?;
     let skip_fetch = match self.no_fetch {
       Some(it) => it,
@@ -266,9 +295,11 @@ impl Args {
     };
 
     if !skip_fetch {
+      progress.set_message("Fetching all remotes");
       fetch_all(repo)?;
     }
 
+    progress.set_message("Updating branches");
     let current_branch = get_current_branch_name(repo)?;
     let branches = repo.branches(Some(BranchType::Local))?;
 
@@ -294,23 +325,29 @@ impl Args {
     };
 
     if !skip_prune {
+      progress.set_message("Pruning branches");
       let results = prune_branches(repo, config, self.dry_run)?;
       for action in results {
         updates.push(action);
       }
     }
 
+    progress.finish_with_message("Synced");
     Ok(SyncAction::Sync(updates))
   }
 
-  fn sync_or_clone_project(&self, project: &ProjectEntry) -> Result<SyncAction> {
+  fn sync_or_clone_project(
+    &self,
+    project: &ProjectEntry,
+    progress: &ProgressBar,
+  ) -> Result<SyncAction> {
     match Repository::open(&project.path) {
       // already cloned, sync it
       Ok(repo) => {
         let app_config = config::load_with_path(&project.path)?;
         let mut git_config = repo.config()?;
         git_config.set_bool("feature.project", true)?;
-        self.sync_repo(&repo, &app_config)
+        self.sync_repo(&repo, &app_config, progress)
       }
 
       // doesn't exist, just clone and continue
@@ -318,6 +355,9 @@ impl Args {
         if (e.class() == ErrorClass::Os && e.code() == ErrorCode::NotFound)
           || (e.class() == ErrorClass::Repository && e.code() == ErrorCode::NotFound) =>
       {
+        progress.set_message("Cloning project");
+        progress.enable_steady_tick(Duration::from_millis(100));
+
         // make sure path exists
         match fs::create_dir_all(&project.path) {
           Ok(_) => (),
@@ -335,6 +375,8 @@ impl Args {
         let repo = builder.clone(&project.url, &project.path)?;
         let mut config = repo.config()?;
         config.set_bool("feature.project", true)?;
+
+        progress.finish_with_message("Cloned project");
         Ok(SyncAction::ProjectInit)
       }
 
@@ -344,7 +386,12 @@ impl Args {
 
   /// Syncs a module similar to `git submodule update --init` and `git submodule
   /// sync`.
-  fn update_module(&self, repo: &Repository, mod_name: &str) -> Result<Option<SyncAction>> {
+  fn update_module(
+    &self,
+    repo: &Repository,
+    mod_name: &str,
+    progress: &ProgressBar,
+  ) -> Result<SyncAction> {
     let mut module = repo.find_submodule(mod_name)?;
     let mod_repo = match module.open() {
       Ok(repo) => Some(repo),
@@ -356,6 +403,9 @@ impl Args {
       }
       Err(e) => return Err(e.into()),
     };
+
+    progress.set_message("Updating module");
+    progress.enable_steady_tick(Duration::from_millis(100));
 
     // if repo, head_id, and index_id all exist, then get expected and actual commit
     // (short ids)
@@ -394,15 +444,18 @@ impl Args {
     // sync submodule's remote url
     module.sync()?;
 
-    // if we failed to get expected/actual before, assume it was just initialized
     if let (Some(expected), Some(actual)) = (expected, actual) {
-      Ok(Some(SyncAction::ModuleUpdate {
-        old: actual,
-        new: expected,
-      }))
+      progress.finish_with_message(format!(
+        "Updated module {} -> {}",
+        style(&actual).yellow(),
+        style(&expected).yellow()
+      ));
     } else {
-      Ok(Some(SyncAction::ModuleInit))
+      // if we failed to get expected/actual before, assume it was just initialized
+      progress.finish_with_message("Initialized module");
     }
+
+    Ok(SyncAction::ModuleUpdate)
   }
 
   /// Fast-forwards a branch to match upstream. Set `current` to true when
@@ -485,54 +538,55 @@ impl Args {
   }
 }
 
-pub fn display_sync_action(name: &str, action: &SyncAction) -> String {
-  match action {
-    SyncAction::Sync(updates) => {
-      let updates: Vec<_> = updates
-        .iter()
-        .filter_map(|update| {
-          if let UpdateAction::None = update {
-            None
-          } else {
-            Some(format!("  {}", update))
-          }
-        })
-        .collect();
+/// Adds a new unstyled spinner to the [MultiProgress]. Returns the newly
+/// created spinner.
+fn add_spinner(multi: &MultiProgress, name: String) -> ProgressBar {
+  let spinner = multi.add(ProgressBar::new_spinner());
+  spinner.set_prefix(name);
+  spinner
+}
 
-      let msg = if updates.is_empty() {
-        " up to date".to_string()
-      } else {
-        format!("\n{}", updates.join("\n"))
-      };
+/// Sets the style of the spinner
+fn set_spinner_style(progress: &ProgressBar, prefix_width: usize) {
+  progress.set_style(
+    ProgressStyle::with_template(&format!(
+      "{{prefix:<{prefix_width}.cyan}} {{spinner:.green}} {{elapsed:.dim}} {{msg}}"
+    ))
+    .expect("Invalid format for progress bar template")
+    .tick_strings(&TICK_STRINGS),
+  );
+}
 
-      format!(
-        "{} {}:{}",
-        style("Synced").bold().green(),
-        style(name).cyan(),
-        msg
-      )
-    }
+/// Displays a list of [UpdateAction]s along with a header line containing
+/// `name`. [UpdateAction::None] is filtered out first. If there are no
+/// remaning updates, an empty string is returned.
+pub fn display_sync_updates(name: &str, updates: &[UpdateAction]) -> String {
+  use std::fmt::Write;
+  let mut out = String::new();
 
-    SyncAction::ProjectInit => format!(
-      "{} project {}",
-      style("Cloned").bold().green(),
-      style(name).cyan()
-    ),
+  let updates: Vec<&UpdateAction> = updates
+    .iter()
+    .filter(|update| match update {
+      UpdateAction::Update { .. }
+      | UpdateAction::UpdateSkip { .. }
+      | UpdateAction::Delete { .. }
+      | UpdateAction::DeleteSkip { .. }
+      | UpdateAction::Err { .. } => true,
 
-    SyncAction::ModuleInit => format!(
-      "{} module {}",
-      style("Initialized").bold().green(),
-      style(name).cyan()
-    ),
+      // filter out None action
+      UpdateAction::None => false,
+    })
+    .collect();
 
-    SyncAction::ModuleUpdate { old, new } => format!(
-      "{} module {} to {} {}",
-      style("Updated").bold().green(),
-      style(name).cyan(),
-      new,
-      style!("(was {})", old)
-    ),
+  if !updates.is_empty() {
+    let _ = write!(out, "{} changes:", style(name).cyan());
   }
+
+  for update in updates {
+    let _ = write!(out, "\n  {}", update);
+  }
+
+  out
 }
 
 impl Display for UpdateAction {
