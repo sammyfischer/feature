7f6c04ed9b71128180d6661644a98bb40f82d992
Sammy Fischer <sammyfischer50@gmail.com>
2026-07-01 16:37:23 -0400

    fix(completions): update bash completions
    Remove deprecated commands log and graph, rename list to branches, add
    new tag-related commands.

--- src/config/tag.rs
+++ src/config/tag.rs
@@ -3,5 +3,6 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Default, Serialize, Deserialize, JsonSchema)]
 pub struct TagConfig {
+  /// Require semver tags to be annotated
   pub require_annotated: bool,
 }
