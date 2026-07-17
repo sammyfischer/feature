1ea504205562458840ae6cb042a8d4bc3f25bd48
Sammy Fischer <sammyfischer50@gmail.com>
2026-07-17 15:38:17 -0400

    feat(branches): add glob filter
    When a single branch is matched, display high detail info about it.

--- src/cli/branches.rs
+++ src/cli/branches.rs
@@ -9,9 +9,10 @@ use crate::cli::display::display_plus_minus;
 use crate::cli::display::time::{DisplayTimeOptions, display_time};
 use crate::cli::term::{get_term_width, is_term};
 use crate::core::branch::{get_current_branch_name, get_worktree_branch_names};
+use crate::core::branch_info::BranchInfo;
 use crate::core::string::{ToStrLossy, ToStrLossyOwned};
 use crate::core::user_config::UserConfig;
-use crate::core::{NotFoundExt, open_repo_from_dirs};
+use crate::core::{NotFoundExt, open_repo_from_dirs, trim_hash};
 use crate::{App, style};
 
 #[derive(clap::Args, Clone, Debug)]
@@ -24,6 +25,10 @@ pub struct Args {
   /// Hides git submodules from output
   #[arg(short = 'M', long, value_name = "HIDE", num_args = 0..=1, require_equals = true, default_missing_value = "true")]
   no_modules: Option<bool>,
+
+  /// Glob pattern to filter branch names
+  #[arg(value_name = "GLOB")]
+  pattern: Option<String>,
 }
 
 #[derive(Debug, Default)]
@@ -66,8 +71,7 @@ impl Args {
         let repo = open_repo_from_dirs(&repo_dir, work_dir)?;
         let user_config = UserConfig::new(&repo)?;
 
-        let table = self.build_table(&repo, &user_config)?;
-        self.display_table(table, &user_config)
+        self.display_branches(&repo, &user_config, self.pattern.as_deref())
       });
 
       let proj_thread = scope.spawn(|| {
@@ -82,9 +86,8 @@ impl Args {
               let repo = Repository::open(&project.path)?;
               let user_config = UserConfig::new(&repo)?;
 
-              let table = self.build_table(&repo, &user_config)?;
               out.push('\n');
-              out.push_str(&self.display_table(table, &user_config)?);
+              out.push_str(&self.display_branches(&repo, &user_config, self.pattern.as_deref())?);
               Ok(out)
             })
             .collect()
@@ -101,12 +104,11 @@ impl Args {
               let repo = open_repo_from_dirs(&repo_dir, work_dir)?;
               let user_config = UserConfig::new(&repo)?;
               let module = repo.find_submodule(name)?;
-              let mod_repo = module.open()?;
+              let repo = module.open()?;
 
               let mut out = format!("\n{} {}", style("Module").bold(), style(name).cyan());
-              let table = self.build_table(&mod_repo, &user_config)?;
               out.push('\n');
-              out.push_str(&self.display_table(table, &user_config)?);
+              out.push_str(&self.display_branches(&repo, &user_config, self.pattern.as_deref())?);
               Ok(out)
             })
             .collect()
@@ -154,6 +156,50 @@ impl Args {
     Ok(())
   }
 
+  /// Gets the list of branches and decides whether to display in single or list
+  /// mode. Returns the entire formatted output.
+  fn display_branches(
+    &self,
+    repo: &Repository,
+    user_config: &UserConfig,
+    glob: Option<&str>,
+  ) -> Result<String> {
+    let branches: Vec<Branch> = if let Some(glob) = glob {
+      repo
+        .references_glob(&format!("refs/heads/{}", glob))?
+        .flatten()
+        .filter_map(|rf| {
+          // with the above glob pattern this should always be true, but it's safe to
+          // double check
+          if rf.is_branch() {
+            Some(Branch::wrap(rf))
+          } else {
+            None
+          }
+        })
+        .collect()
+    } else {
+      repo
+        .branches(Some(BranchType::Local))?
+        .flatten()
+        .map(|(branch, _)| branch)
+        .collect()
+    };
+
+    if branches.len() == 1 {
+      self.display_single_branch(repo, user_config, &branches[0])
+    } else {
+      let mut table = Vec::with_capacity(branches.len());
+      for branch in branches {
+        let row = self.build_row(repo, user_config, &branch)?;
+        table.push(row);
+      }
+
+      self.display_table(table, user_config)
+    }
+  }
+
+  /// Create a row in the table from a branch
   fn build_row(&self, repo: &Repository, user_config: &UserConfig, branch: &Branch) -> Result<Row> {
     let mut row = Row::default();
 
@@ -161,17 +207,19 @@ impl Args {
     let current = get_current_branch_name(repo)?;
     let wt_branches = get_worktree_branch_names(repo)?;
 
+    let trunc_name = truncate_str(&branch_name, 30, "\u{2026}");
+
     row.branch = if current.is_some_and(|current| current == branch_name) {
       // highlight checked-out branch green
-      style(&branch_name).bold().green().to_string()
+      style(&trunc_name).bold().green().to_string()
     } else if wt_branches
       .iter()
       .any(|wt_branch| wt_branch == &branch_name)
     {
       // highlight checked-out worktree branches cyan
-      style(&branch_name).bold().cyan().to_string()
+      style(&trunc_name).bold().cyan().to_string()
     } else {
-      style(&branch_name).bold().to_string()
+      style(&trunc_name).bold().to_string()
     };
 
     let branch_commit = branch.get().peel_to_commit()?;
@@ -226,20 +274,8 @@ impl Args {
     Ok(row)
   }
 
-  fn build_table(&self, repo: &Repository, user_config: &UserConfig) -> Result<Vec<Row>> {
-    let mut table = Vec::new();
-
-    let branches = repo.branches(Some(BranchType::Local))?;
-    for (branch, _) in branches.flatten() {
-      let row = self.build_row(repo, user_config, &branch)?;
-      table.push(row);
-    }
-
-    Ok(table)
-  }
-
-  /// Calculates the width of the left column, which includes the name and
-  /// ahead/behind counts
+  /// Calculates the width of the left column of branch list, which includes the
+  /// name and ahead/behind counts. The width is used to align the commit text.
   fn calculate_column_width(&self, table: &Vec<Row>) -> usize {
     let mut width = 0usize;
 
@@ -271,6 +307,7 @@ impl Args {
     width
   }
 
+  /// Displays a list of branches in a somewhat tabular format
   fn display_table(&self, table: Vec<Row>, user_config: &UserConfig) -> Result<String> {
     use std::fmt::Write;
     let mut out = String::new();
@@ -319,6 +356,164 @@ impl Args {
       ab_buf.clear();
 
       write!(out, " {}", &row.commit)?;
+    }
+
+    Ok(out)
+  }
+
+  /// If a single branch is matched, display in high detail
+  fn display_single_branch(
+    &self,
+    repo: &Repository,
+    user_config: &UserConfig,
+    branch: &Branch,
+  ) -> Result<String> {
+    use std::fmt::Write;
+    let mut out = String::with_capacity(100);
+
+    let info = BranchInfo::from_branch(branch)?;
+
+    // highlight name the same way as list mode
+    let current = get_current_branch_name(repo)?;
+    let wt_branches = get_worktree_branch_names(repo)?;
+
+    if current.is_some_and(|it| it == info.name()) {
+      write!(out, "{}", style(info.name()).green())?;
+    } else if wt_branches.iter().any(|it| it == info.name()) {
+      write!(out, "{}", style(info.name()).cyan())?;
+    } else {
+      write!(out, "{}", info.name())?;
+    }
+
+    // branch-name
+    //
+    //  355daf4 Author Name, 15 hours ago
+    //  fix(stash): add status output after stash pop
+    //
+    //  Base     origin/branch-status +0 -4
+    //  Upstream origin/main          +0 -0
+    //
+    // no nerd font:
+    // branch-name
+    //
+    // 355daf4 Author Name, 15 hours ago
+    // fix(stash): add status output after stash pop
+    //
+    // Base     origin/branch-status +0 -4
+    // Upstream origin/main          +0 -0
+
+    let commit = branch.get().peel_to_commit()?;
+
+    let nerd_font = user_config.nerd_font()?;
+    write!(
+      out,
+      "\n\n{}",
+      style!(
+        "{}{}",
+        if nerd_font { " " } else { "" },
+        trim_hash(commit.as_object())?
+      )
+      .yellow()
+    )?;
+
+    write!(
+      out,
+      " {}, {}",
+      commit.author().name()?,
+      display_time(&commit.time(), &DisplayTimeOptions {
+        relative: user_config.format_relative()?,
+        fmt: user_config.format_date()?
+      })?
+    )?;
+
+    write!(
+      out,
+      "\n{}",
+      style!(
+        "{}{}",
+        if nerd_font { " " } else { "" },
+        commit.summary()?.expect("Commit should have a summary")
+      )
+      .dim()
+    )?;
+
+    /// Branch ahead/behind is printed as a table:
+    ///  Upstream origin/main          +0 -0
+    ///  Base     origin/branch-status +0 -4
+    ///
+    /// This represents a row in that table
+    struct BranchRow {
+      /// Upstream or base, possibly with the icon
+      label: String,
+
+      /// Branch name
+      name: String,
+
+      /// Ahead/behind this branch (upstream/base) vs. the branch being listed
+      ab: (usize, usize),
+    }
+
+    let mut branch_rows = Vec::with_capacity(2);
+
+    if let Some(upstream) = branch.upstream().not_found_ok()? {
+      let upstream_tip = upstream.get().peel_to_commit()?.id();
+      let ab = repo.graph_ahead_behind(upstream_tip, commit.id())?;
+
+      let upstream_row = BranchRow {
+        label: style!("{}{}", if nerd_font { " " } else { "" }, "Upstream")
+          .blue()
+          .to_string(),
+        name: upstream
+          .name()?
+          .expect("Upstream should have a name")
+          .to_string(),
+        ab,
+      };
+
+      branch_rows.push(upstream_row);
+    }
+
+    if let Some(base) = user_config.branch_base(info.name())? {
+      let base_tip = base.resolve(repo)?.peel_to_commit()?.id();
+      let ab = repo.graph_ahead_behind(base_tip, commit.id())?;
+
+      let base_row = BranchRow {
+        label: style!("{}{}", if nerd_font { " " } else { "" }, "Base")
+          .magenta()
+          .to_string(),
+        name: base.name().to_string(),
+        ab,
+      };
+
+      branch_rows.push(base_row);
+    }
+
+    let mut label_width = 0usize;
+    let mut name_width = 0usize;
+
+    for row in &branch_rows {
+      label_width = label_width.max(measure_text_width(&row.label));
+      name_width = name_width.max(measure_text_width(&row.name));
+    }
+
+    if !branch_rows.is_empty() {
+      // double space
+      writeln!(out)?;
+    }
+
+    for row in &branch_rows {
+      let label = {
+        let padding = label_width - measure_text_width(&row.label);
+        format!("{}{}", row.label, " ".repeat(padding))
+      };
+
+      let name = {
+        let padding = name_width - measure_text_width(&row.name);
+        format!("{}{}", row.name, " ".repeat(padding))
+      };
+
+      let (a, b) = row.ab;
+      write!(out, "\n{} {} {}", label, name, display_plus_minus(a, b))?;
     }
 
     Ok(out)
